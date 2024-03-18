b11c0a09477d8ee54b759a3ebfd6ad09214b9d35
MadanStha7 <madanstha.124@gmail.com>
2024-03-18 10:48:29 +0545

    Initail commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://pzvzuovosmxjxfkpypgt.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6dnp1b3Zvc214anhma3B5cGd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNDE1ODgsImV4cCI6MjAyNTkxNzU4OH0.bsAfx4yvMV0qULMRLq3q5XGa61HMFuUVCYERAWT_jnM';
+}
