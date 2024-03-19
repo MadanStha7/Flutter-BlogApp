fc56f41d6990e83b9b38047ac84ec8f5c07d3285
MadanStha7 <madanstha.124@gmail.com>
2024-03-19 23:13:31 +0545

    Blog card and viewer

---
+++ lib/features/blog/presentation/pages/blog_viewer_page.dart
@@ -0,0 +1,54 @@
+import 'package:blog_app/core/utils/calculate_reading_time.dart';
+import 'package:blog_app/features/blog/domain/entities/blog.dart';
+import 'package:flutter/material.dart';
+
+class BlogViewerPage extends StatelessWidget {
+  static route(Blog blog) => MaterialPageRoute(
+        builder: (context) => BlogViewerPage(blog: blog),
+      );
+  final Blog blog;
+  const BlogViewerPage({super.key, required this.blog});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          child: const Icon(
+            Icons.arrow_back_ios,
+          ),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              blog.title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'By ${blog.posterName}',
+              style: const TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              '${blog.updatedAt} . ${calculateReadingTime(blog.content)} min',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
